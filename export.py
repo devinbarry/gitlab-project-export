68edf32398114df1feab3d89e213b7f1ad779fe5
Devin Barry <devin@hyperelliptic.xyz>
2023-05-06 02:44:06 +0100

    Cleanup. Refactor to use new GitlabClient class. Bug fixes.

--- export.py
+++ export.py
@@ -47,76 +47,75 @@ def prepare_config_variables(c):
     gitlab_url = c.config["gitlab"]["access"]["gitlab_url"]
     ssl_verify = c.config["gitlab"]["access"]["ssl_verify"]
     wait_between_exports = c.config['gitlab'].get('wait_between_exports', 0)
-    membership = c.config['gitlab'].get('membership', True)
-    include_archived = c.config['gitlab'].get('include_archived', False)
     max_tries_number = c.config['gitlab'].get('max_tries_number', 12)
     retention_period = c.config['backup'].get('retention_period', 0)
-    exclude_projects = c.config["gitlab"].get('exclude_projects', [])
 
     if not (isinstance(retention_period, (int, float)) and retention_period >= 0):
         print("Invalid value for retention_period. ignoring")
         retention_period = 0
 
-    return (token, gitlab_url, ssl_verify, wait_between_exports, membership, include_archived, max_tries_number,
-            retention_period, exclude_projects)
+    return token, gitlab_url, ssl_verify, wait_between_exports, max_tries_number, retention_period
 
 
-def init_gitlab_api(gitlab_url, token, ssl_verify):
-    if debug:
-        print(f"{gitlab_url}, token")
-    return GitlabClient(gitlab_url, token, ssl_verify)
+def get_projects_to_export(gitlab_client, c):
+    membership = c.config['gitlab'].get('membership', True)
+    archived = c.config['gitlab'].get('include_archived', False)
+    exclude_projects = c.config["gitlab"].get('exclude_projects', [])
 
-
-def get_projects_to_export(gitlab, c, membership, include_archived, exclude_projects):
     export_projects = []
-    projects = gitlab.project_list(membership=str(membership), archived=str(include_archived))
-    if not projects:
+    all_projects = gitlab_client.list_all_projects(membership=str(membership), archived=str(archived))
+    if not all_projects:
         print("Unable to get projects for your account", file=sys.stderr)
         sys.exit(1)
 
-    # Create export_projects array
+    # Select all projects that match the project pattern
     for project_pattern in c.config["gitlab"]["projects"]:
-        for gitlab_project in projects:
+        for gitlab_project in all_projects.keys():
             if re.match(project_pattern, gitlab_project):
                 export_projects.append(gitlab_project)
 
     # Remove any projects that are marked as excluded
     for ignored_project_pattern in exclude_projects:
-        for gitlab_project in projects:
+        for gitlab_project in all_projects.keys():
             if re.match(ignored_project_pattern, gitlab_project):
                 if debug:
                     print(f"Removing project '{gitlab_project}' from export (exclusion: '{ignored_project_pattern}'): ")
                 export_projects.remove(gitlab_project)
 
-    return export_projects
+    # Create a map of project names to project IDs as the final response
+    output = {}
+    for project in export_projects:
+        output[project] = all_projects[project]
+    return output
 
-def create_project_directory(c, project):
-    destination = c.config["backup"]["destination"]
+def create_project_directory(c, project_name):
+    project_dir = c.config["backup"]["destination"]
     if c.config["backup"]["project_dirs"]:
-        destination += f"/{project}"
+        project_dir += f"/{project_name}"
 
     try:
-        os.makedirs(destination, exist_ok=True)
+        os.makedirs(project_dir, exist_ok=True)
     except OSError as e:
-        print(f"Unable to create directories {destination}: {e}", file=sys.stderr)
+        print(f"Unable to create directories {project_dir}: {e}", file=sys.stderr)
         sys.exit(1)
 
-    return destination
+    return project_dir
 
-def prepare_destination_file(c, project, destination):
+def _create_file_name(c, project_name, project_dir):
     d = datetime.now()
     file_tmpl = c.config["backup"]["backup_name"]
-    dest_file = destination + "/" + file_tmpl.replace("{PROJECT_NAME}", project.replace("/", "-"))
+    dest_file = project_dir + "/" + file_tmpl.replace("{PROJECT_NAME}", project_name.replace("/", "-"))
     dest_file = dest_file.replace("{TIME}", d.strftime(c.config["backup"]["backup_time_format"].replace(" ", "_")))
 
     return dest_file
 
-def export_project(args, gitlab, project, destination, dest_file, max_tries_number, ssl_verify, token,
-                   retention_period, wait_between_exports, export_projects):
+def export_project(client, project_id, project_name, project_dir, dest_file, ssl_verify, token, retention_period, args):
     global return_code
 
+    download_url = client.export_project(project_id=project_id)
+
     if debug:
-        print(f" Destination {destination}")
+        print(f"Destination {project_dir}")
 
     if os.path.isfile(dest_file):
         if not args.force:
@@ -128,49 +127,43 @@ def export_project(args, gitlab, project, destination, dest_file, max_tries_numb
             os.remove(dest_file)
 
     if retention_period > 0:
-        purge_old_files(destination, retention_period)
+        purge_old_files(project_dir, retention_period)
 
-    status = gitlab.project_export(project, max_tries_number)
-
-    if status:
-        return_code += download_exported_project(gitlab, project, dest_file, ssl_verify, token)
+    if download_url is not None:
+        if debug:
+            print(f"Successfully exported for {project_name}")
+        return_code += download_exported_project(download_url, project_name, dest_file, ssl_verify, token)
     else:
-        print(f"Export failed for project {project}", file=sys.stderr)
+        print(f"Export failed for project {project_name}", file=sys.stderr)
         return_code += 1
 
-    if project != export_projects[-1]:
-        if debug:
-            print(f"Waiting between exports for {wait_between_exports} seconds")
-        time.sleep(wait_between_exports)
 
-def purge_old_files(destination, retention_period):
+
+def purge_old_files(project_dir, retention_period):
     if debug:
-        print(f" Purging files older than {retention_period:.2f} days in the folder {destination}")
+        print(f" Purging files older than {retention_period:.2f} days in the folder {project_dir}")
 
     now = time.time()
-    for f in os.listdir(destination):
+    for f in os.listdir(project_dir):
         if not f.endswith(".tar.gz"):
             continue
 
-        f = os.path.join(destination, f)
+        f = os.path.join(project_dir, f)
         if os.path.isfile(f):
             if os.stat(f).st_mtime < now - (retention_period * 86400):
                 if debug:
                     print(f"   deleting backup {f}")
                 os.remove(f)
 
-def download_exported_project(gitlab, project, dest_file, ssl_verify, token):
+def download_exported_project(download_url, project, dest_file, ssl_verify, token):
     """
     function returns either 0 (success) or 1 (failure).
     """
     if debug:
-        print(f"Success for {project}")
+        print(f"Download URL: {download_url}")
 
-    url = gitlab.download_url["api_url"]
-    if debug:
-        print(f" URL: {url}")
-
-    r = requests.get(url, allow_redirects=True, stream=True, verify=ssl_verify, headers={"PRIVATE-TOKEN": token})
+    r = requests.get(download_url, allow_redirects=True, stream=True, verify=ssl_verify,
+                     headers={"PRIVATE-TOKEN": token})
 
     if 200 <= r.status_code < 300:
         with open(dest_file, 'wb') as f:
@@ -197,9 +190,13 @@ def main():
 
     c = config.Config(args.config)
 
-    token, gitlab_url, ssl_verify, wait_between_exports, membership, include_archived, max_tries_number, retention_period, exclude_projects = prepare_config_variables(c)
-    gitlab = init_gitlab_api(gitlab_url, token, ssl_verify)
-    export_projects = get_projects_to_export(gitlab, c, membership, include_archived, exclude_projects)
+    token, gitlab_url, ssl_verify, wait_between_exports, max_tries_number, retention_period = prepare_config_variables(c)
+
+    if debug:
+        print(f"Initialising GitlabClient with URL: {gitlab_url}")
+    client = GitlabClient(gitlab_url, token, ssl_verify)
+
+    export_projects = get_projects_to_export(gitlab_client=client, c=c)
 
     if debug:
         print(f"Projects to export: {export_projects}")
@@ -208,17 +205,22 @@ def main():
         print("Not running actual export because of -n/--noop flag.")
         sys.exit(0)
 
-    for project in export_projects:
+    for project_name, project_id in export_projects.items():
         if debug:
-            print(f"Exporting {project}")
+            print(f"Exporting {project_name} - {project_id}")
 
-        destination = create_project_directory(c, project)
-        dest_file = prepare_destination_file(c, project, destination)
+        project_dir = create_project_directory(c, project_name)
+        dest_file = _create_file_name(c, project_name, project_dir)
 
         if debug:
             print(f" Destination file {dest_file}")
 
-        export_project(args, gitlab, project, destination, dest_file, max_tries_number, ssl_verify, token, retention_period, wait_between_exports, export_projects)
+        export_project(client, project_id, project_name, project_dir, dest_file, ssl_verify, token, retention_period,
+                       args)
+
+        if debug:
+            print(f"Waiting between exports for {wait_between_exports} seconds")
+        time.sleep(wait_between_exports)
 
     sys.exit(return_code)
 
